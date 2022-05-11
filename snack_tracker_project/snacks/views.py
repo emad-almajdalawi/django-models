f3fd1dc50cb96dba58c68f5a4d57b776abf6d894
emad-almajdalawi <majdalawi97@yahoo.com>
2022-05-11 18:17:16 +0300

    details

--- snack_tracker_project/snacks/views.py
+++ snack_tracker_project/snacks/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, DetailView
+from .models import Snack
 
 class HomeView(TemplateView):
-    template_name = ''
+    template_name = 'home.html'
+
 
 class SnackListView(ListView):
     template_name = 'snack_list.html'
+    model = Snack
+    context_object_name = 'snack_object'
+
+    # def get_queryset(self):
+    #     return models.Snack.objects.all()
+
+class SnackDeatailView(DetailView):
+    template_name = 'snack_detail.html'
+    model = Snack
+
+
 
